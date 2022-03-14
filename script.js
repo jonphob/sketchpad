b6abe3aa0ae5f32fd2af8f215ccd019e58972689
Jon Flitcroft <jondenise@gmail.com>
2022-03-14 17:31:53 +0000

    Added function on mouseenter to create drawing effect

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const sketchContainer = document.querySelector(".sketch-area");
+const slider = document.getElementById("gridSizeSlider");
+const gridSizeDisplay = document.getElementById("size-display");
+
+function clearGrid(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+function addColor() {
+  this.style.backgroundColor = "black";
+}
 
 const createGrid = (gridSize) => {
   for (let i = 0; i < Math.pow(gridSize, 2); i++) {
@@ -6,10 +18,20 @@ const createGrid = (gridSize) => {
     square.classList.add("square");
     sketchContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
     sketchContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-    square.style.width = `${960 / gridSize}px`;
-    square.style.height = `${960 / gridSize}px`;
+    square.style.width = `${1000 / gridSize}px`;
+    square.style.height = `${1000 / gridSize}px`;
     sketchContainer.appendChild(square);
+    square.addEventListener("mouseenter", addColor);
   }
 };
 
-createGrid(16);
+gridSizeDisplay.innerText = `${slider.value} x ${slider.value}`;
+
+slider.addEventListener("input", (e) => {
+  clearGrid(sketchContainer);
+  createGrid(e.target.value);
+
+  gridSizeDisplay.innerText = `${e.target.value} x ${e.target.value}`;
+});
+
+createGrid(50);
